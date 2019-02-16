71d4061d3b94ca0c509d0245a676342728b46ca3
chengqi77 <2113265828@qq.com>
2019-02-16 17:58:34 +0800

    -

--- src/pages/share/main.js
+++ src/pages/share/main.js
@@ -1,13 +1,14 @@
 
 
 import { Component } from '@tarojs/taro';
-import { View, ScrollView, TTabs, TTabPane, Text,TInput } from '../../components'
+import { View, ScrollView, TTabs, TTabPane, Text, TInput } from '../../components'
 import Item from './item';
 
 import './main.scss';
+import imgs from './img/logo.png'
 
 const item = {
-   
+
 };
 
 
@@ -15,11 +16,11 @@ export default class MyDemand extends Component {
 
 
     state = {
-        title:"",
-        title1:"",
-        content:"",
+        title: "",
+        title1: "",
+        content: "",
         current: 0,
-        url:"",
+        url: "../share/img/logo.png"
     };
     componentWillReceiveProps(nextProps) {
 
@@ -34,58 +35,69 @@ export default class MyDemand extends Component {
         this.setState({
             current
         });
-       
+
     }
-    onchangeInput(title){
-        if(title.length >10){
-            return  
+    onchangeInput(title) {
+        if (title.length > 10) {
+            return
         }
-        else{
+        else {
             this.setState({
                 title
             });
         }
-      
-       // this.state.title1 = title 
+
+        // this.state.title1 = title 
     }
-    onchangecontent(content){
-        if(content.length >20){
-            return  
+    onchangecontent(content) {
+        if (content.length > 20) {
+            return
         }
-        else{
+        else {
             this.setState({
                 content
             });
         }
-      
-       // this.state.title1 = title 
+
+        // this.state.title1 = title 
     }
     render() {
-        const { list, itemDescList, itemKeyList, current ,title,content} = this.state;
-       // const tabList = ["我的需求", "我的报价"];
+        const { list, itemDescList, itemKeyList, current, title, content } = this.state;
+        // const tabList = ["我的需求", "我的报价"];
         return (
             <View className='container'>
-            <Text className="tit">自定义分享链接内容</Text>
+                <Text className="tit">自定义分享链接内容</Text>
                 <View className="box">
-                <Text>标题</Text>
-                <TInput className="tinput" value={title} onInput={this.onchangeInput} />
+                    <Text>标题</Text>
+                    <TInput className="tinput" value={title} onInput={this.onchangeInput} />
                 </View>
-                <View>
-                <Text>内容</Text>
-                <TInput value={content} onInput={this.onchangecontent}/>
+                <View className="box">
+                    <Text>内容</Text>
+                    <view className="tinput">
+                    <TInput className="tinput" value={content} onInput={this.onchangecontent} />                    
+                    </view>
                 </View>
+                <view className="line"></view>
                 <View>
-                <Text className="">分享说明：</Text>
-                <View>
-                <Text className="">1.自定义分享微信朋友圈/好友卡片标题，填写对应展示如图</Text>
-                <Text className="">2.精选批次，请先筛选后在进行分享</Text>
-                </View>
-                <View>
-                <Text>字数同步啊</Text>
-                 <Text className="">{title}</Text>
-                 <Text className="">{content}</Text>
-                 <Image src={url} />
-                </View>
+                    <Text className="">分享说明：</Text>
+                    <View>
+                        <Text className="">1.自定义分享微信朋友圈/好友卡片标题，填写对应展示如图</Text>
+                        <view />
+                        <view>
+                            <Text className="">2.精选批次，请先筛选后在进行分享</Text>
+                        </view>
+                    </View>
+                    <View className="card">
+                        <view className="cardTit">
+                            <Text className="">{title}</Text>
+                        </view>
+                        <view className="cardCon">
+                            <Text className="">{content}</Text>
+                        </view>
+                        <view className="cardlogos">
+                            <Image src={imgs} className="logosImg" />
+                        </view>
+                    </View>
                 </View>
             </View>
         )
