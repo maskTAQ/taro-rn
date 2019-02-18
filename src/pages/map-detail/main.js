1cc4ecf6b06a962ec00c0dd72a53969cebb5c746
chengqi77 <2113265828@qq.com>
2019-02-18 23:01:28 +0800

    地图页面

--- src/pages/map-detail/main.js
+++ src/pages/map-detail/main.js
@@ -5,18 +5,49 @@ import { View, Text, TInput, Image, TButton } from '../../components';
 import { Map } from '@tarojs/components'
 
 import './main.scss';
-import imgs from './img/logo.png'
+import ip from './img/ip.png'
+import fk from './img/fk.png'
+import icon from './img/icon.png'
+
 
 export default class MapDetail extends Component {
 
 
     state = {
-
+        latitude: 23.099994,
+        longitude: 113.324520,
+        markers: [{
+            iconPath: icon,
+            id: 0,
+            latitude: 23.099994,
+            longitude: 113.324520,
+            width: 30,
+            height: 30
+        }],
     };
 
+    moveToLocation = () => {
+        console.log('回到初始位置')
+        this.mapCtx.moveToLocation()
+        
+       // let mpCtx = wx.createMapContext("myMap");
+            // mpCtx.showToLocation();
+ 
+        // this.setState({
+        //     latitude:23.099994,
+        //     longitude:113.324520,
+        //     // markers:[
+        //     //     {
+        //     //         latitude:23.099994,
+        //     //         longitude:113.324520,
+        //     //     }
+        //     // ],
+        // });
+
+    }
 
     render() {
-        const { } = this.state;
+        const { longitude, latitude, markers } = this.state;
 
         return (
             <View className='container'>
@@ -29,9 +60,20 @@ export default class MapDetail extends Component {
                 <View className="line-container">
                     <Text className="laber">联系方式：</Text>
                     <Text className="content">我是标题中棉库存有限责任公司</Text></View>
-                <view className="map">
-                    <Map className="maps" onClick={this.onTap} />
-                </view>
+                <View className="map">
+
+                    <Map longitude={longitude} id="myMap" 
+                        latitude={latitude} markers={markers} className="maps" showLocation />
+                    <View className="map-btn">
+                        <TButton onClick={this.moveToLocation}>
+                            <Image className="map-img" src={ip}></Image>
+                        </TButton>
+                        <TButton>
+                            <Image className="map-img" src={fk}></Image>
+                        </TButton>
+
+                    </View>
+                </View>
             </View>
         )
     }
