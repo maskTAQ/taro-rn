e5c3b4dd82f26a728da56bde0d5487f94a2c6db1
邰爱强 <50889574@qq.com>
2019-02-18 23:01:20 +0800

    绘制报价单

--- src/pages/quotation-list/main.js
+++ src/pages/quotation-list/main.js
@@ -1,5 +1,7 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { Canvas, Button } from '@tarojs/components'
+import Taro, { Component, Config } from '@tarojs/taro';
+import { Canvas, Button } from '@tarojs/components';
+import { AtToast } from "taro-ui"
+
 import './main.scss'
 import Time from './component/time.js'
 
@@ -16,68 +18,110 @@ class Index extends Component {
 
     // 绘制表盘
     getDialClock = () => {
-        const width = this.state.width;
+        const width = 750;
         const height = this.state.height;
         const ctx = Taro.createCanvasContext('myCanvas', this.$scope);
-        const R = width / 2 - 30;//圆半径
-        const r = R - 15;
 
-        //设置坐标轴原点
-        ctx.translate(width / 2, height / 2);
-        ctx.save();
-
-        // 圆心
-        ctx.beginPath();
-        ctx.arc(0, 0, 5, 0, 2 * Math.PI, true);
-        ctx.fill();
-        ctx.closePath();
-
-        // 表盘外圆
-        ctx.setLineWidth(2);
-        ctx.beginPath();
-        ctx.arc(0, 0, R, 0, 2 * Math.PI, true);
-        ctx.closePath();
-        ctx.stroke();
-
-        // 表盘刻度（大格）
-        ctx.beginPath();
-        ctx.setLineWidth(5);
-        for (var i = 0; i < 12; i++) {
-            ctx.beginPath();
-            ctx.rotate(Math.PI / 6);
-            ctx.moveTo(R, 0);
-            ctx.lineTo(r, 0);
-            ctx.stroke();
+        const data = {
+            websiteName: '中棉网',
+            website: 'www.chncot.com',
+            websiteDesc: '聚万千零差价无中介棉花资源信息',
+            companyName: '江苏丰润科技有限公司',
+            contact: '联系人:王先生',
+            contactMobile: '联系电话:13888888888',
+            gzjgLabel: '公重价格:',
+            gzjgValue: '14322',
+            s_tLabel: '生/贴(元/吨):',
+            s_tValue: '30132.1',
+            bz: '标准:郑商所(17/18年度)',
+            remark: '注:安师大纪念卡迪士尼，马到接口拿，代码，'
+        };
+        const layout = {
+            brandImg: { w: 50, h: 50 },
+            websiteName: { fs: 24, x: 10 },
+            website: { fs: 16, x: 10 },
+            websiteDesc: { fs: 16, x: 10 },
+            companyName: { fs: 20 },
+            contact: { fs: 20 },
+            contactMobile: { fs: 20 },
+            gzjgLabel: { fs: 22 },
+            gzjgValue: { fs: 30, color: 'red' },
         }
-        ctx.closePath();
+        const drawTable = ctx => {
+            const width = 750 - 40;
+            const lineHeight = 40;
+            let initHeight = 280;
+            ctx.strokeRect(0, initHeight, 2, 1000);
+            ctx.strokeRect(width / 2, initHeight, 2, 1000);
+            ctx.strokeRect(width, initHeight, 2, 1000);
+            ctx.strokeRect(width / 2 / 5 * 2, initHeight + lineHeight, 2, lineHeight * 4);
+            ctx.strokeRect(width / 10 * 7, initHeight + lineHeight, 2, lineHeight * 4);
 
-        // 表盘刻度（小格）
-        ctx.beginPath();
-        ctx.setLineWidth(1);
-        for (var i = 0; i < 60; i++) {
-            ctx.beginPath();
-            ctx.rotate(Math.PI / 30);
-            ctx.moveTo(R, 0);
-            ctx.lineTo(R - 10, 0);
-            ctx.stroke();
+            ctx.strokeRect(width / 2 / 7 * 2, initHeight + lineHeight * 5, 2, lineHeight * 10);
+
+            for (let i = 0; i <= 7; i++) {
+                ctx.strokeRect(0, initHeight, width, 2);
+                initHeight += lineHeight;
+            }
+
         }
-        ctx.closePath();
+        const scale = this.state.width / 750;
+        ctx.scale(scale, scale);
 
-        // 表盘时刻（数字）
-        ctx.beginPath();
-        ctx.setFontSize(16)//设置字体样式
-        // ctx.setTextBaseline("middle");//字体上下居中，绘制时间
-        for (let i = 1; i < 13; i++) {
-            //利用三角函数计算字体坐标表达式
-            var x = (r - 10) * Math.cos(i * Math.PI / 6 - Math.PI / 2);
-            var y = (r - 10) * Math.sin(i * Math.PI / 6 - Math.PI / 2);
-            let sz = i + '';
-            ctx.fillText(sz, x - 5, y + 5, 15);
-        }
-        ctx.closePath();
+        ctx.translate(20, 20);
+        //addMethods(ctx);
+        ctx.textBaseline = "hanging";
+        const {
+            brandImg, websiteName, website, websiteDesc,
+            companyName, contactMobile, gzjgLabel, gzjgValue,
+            //s_tLabel,s_tValue,bzLabel,bzValue,remark
+        } = layout;
+        //绘制商标
+        // createImg(brandSrc, function (img) {
+        //     ctx.drawImage(img, 0, 0, brandImg.w, brandImg.h);
+        // });
+        //
+        ctx.setFontSize(websiteName.fs);
+        let l = brandImg.w + websiteName.x;
+        ctx.fillText(data.websiteName, l, 0);
+        ctx.setFontSize(website.fs);
+        l = l + (websiteName.fs * data.websiteName.length) + website.x;
+        ctx.fillText(data.website, l, 6);
+        l = brandImg.w + websiteDesc.x;
+        ctx.fillText(data.websiteDesc, l, 32);
+
+        ctx.setFontSize(companyName.fs);
+        ctx.fillText(data.companyName, 0, 70);
+        ctx.fillText(data.contact, 0, 100);
+        ctx.fillText(data.contactMobile, 0, 130);
+
+        ctx.setFontSize(gzjgLabel.fs);
+        l = contactMobile.fs * data.contactMobile.length
+        ctx.fillText(data.gzjgLabel, l, 130);
+
+        ctx.setFontSize(gzjgValue.fs);
+        ctx.setFillStyle(gzjgValue.color);
+        l = l + gzjgLabel.fs * data.gzjgLabel.length;
+        ctx.fillText(data.gzjgValue, l, 124);
+
+        ctx.strokeRect(0, 176, width - 40, 2);
+
+        ctx.setFontSize(17);
+        ctx.setFillStyle('#000');
+        ctx.fillText(data.s_tLabel, 0, 190);
+        ctx.setFillStyle('green');
+        ctx.fillText(data.s_tValue, 110, 190);
+
+        ctx.setFillStyle('#000');
+        ctx.fillText(data.bz, width - (data.bz.length * 15), 190);
+        ctx.fillText(data.remark, 0, 220);
+
+        ctx.strokeRect(0, 250, width - 40, 2);
+
+        drawTable(ctx);
 
 
-        // 开始绘制
+
         ctx.draw();
     }
 
@@ -97,24 +141,34 @@ class Index extends Component {
         this.getDialClock();
     }
     save = () => {
-        const { width, heght } = this.state;;
+        const { width, height } = this.state;;
         Taro.canvasToTempFilePath({
-            // width: 170, //canvas原本的大小
-            // heght: 170,
-            width, heght,
-            destWidth: 680,  //生成图片的大小设置成canvas大小的四倍
-            destHeight: 680,
+            width, height,
+            destWidth: width * 4,  //生成图片的大小设置成canvas大小的四倍
+            destHeight: height * 4,
             canvasId: 'myCanvas',
             fileType: 'png',
             quality: 1,//图片质量
             success: (res) => {
-                console.log(res);
-                wx.saveImageToPhotosAlbum({
-                    filePath: res.tempFilePath,
-                });
+                const { errMsg } = res;
+                if (errMsg.includes('ok')) {
+                    wx.saveImageToPhotosAlbum({
+                        filePath: res.tempFilePath,
+                    });
+                    Taro.showToast({
+                        title: '保存成功',
+                        icon: 'success',
+                        duration: 2000
+                    });
+                }
+
             },
             fail: function (res) {
-                console.log(res);
+                Taro.showToast({
+                    title: '保存失败',
+                    icon: 'success',
+                    duration: 2000
+                });
             },
 
         }, this.$scope)
